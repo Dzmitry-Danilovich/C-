90283b9ca9354bafc72bba87f90c89dcbf8ae904
Dzmitry_Danilovich <danilovich.dzmitry@icloud.com>
2026-04-28 21:10:05 +0200

    Test task 1.0

--- program.cpp
+++ program.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 
 int main(){
-	cout << "tekst" << endl;
+	float a, b, pole;
+
+	cin >> a;
+	cin >> b;
+
+	pole = a * b;
+	cout << "Pole przed: " << pole << endl;
+
+	cout << "Pole:" << round(pole*100)/100 << endl;
 
 	return 0;
 }
