ea5cc262e6f214623c13bf7f2e242eaa7d52003d
Dzmitry_Danilovich <danilovich.dzmitry@icloud.com>
2026-04-29 20:51:20 +0200

    random Erorr

--- task-1_5.cpp
+++ task-1_5.cpp
@@ -8,7 +8,11 @@ int main(){
     random_device rd;
     mt19937_64 gen(rd());
     uniform_int_distribution<> dist(1, 100); 
-
+    
     int liczba = dist(gen);
     cout << liczba << endl;
+
+    uniform_real_distribution<> dist(0.0, 1.0);
+    float number = dist(gen);
+    cout << number << endl;
 }
