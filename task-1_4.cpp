4cb8669578384c5fd7db6a3e5ddc3c6256e080cf
Dzmitry_Danilovich <danilovich.dzmitry@icloud.com>
2026-04-29 20:23:44 +0200

    Task 1.4

---
+++ task-1_4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main(){
+	auto a = 37, b = 11;
+    cout << "a: " << a << ",   b: " << b << endl;
+    cout << "calkowieta liczba z a / b = " << round(a/b) << endl;
+    cout << "reszta z dzialnia a / b = " << a%b << endl; 
+}
