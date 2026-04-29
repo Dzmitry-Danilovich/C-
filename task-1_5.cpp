dd2341ae99a73f5334bf2b158ef5ff91f852e2f0
Dzmitry_Danilovich <danilovich.dzmitry@icloud.com>
2026-04-29 20:53:03 +0200

    Fix random

--- task-1_5.cpp
+++ task-1_5.cpp
@@ -7,12 +7,12 @@ using namespace std;
 int main(){
     random_device rd;
     mt19937_64 gen(rd());
-    uniform_int_distribution<> dist(1, 100); 
-    
-    int liczba = dist(gen);
+    uniform_int_distribution<> dist_INT(1, 100); 
+    uniform_real_distribution<> dist_FLOAT(0.0, 1.0);
+
+    int liczba = dist_INT(gen);
     cout << liczba << endl;
 
-    uniform_real_distribution<> dist(0.0, 1.0);
-    float number = dist(gen);
+    float number = dist_FLOAT(gen);
     cout << number << endl;
 }
